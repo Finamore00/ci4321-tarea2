0317f6dbe9d600025724f15cebe062228f8347e0
Santiago Finamore <18-10125@usb.ve>
2024-10-06 11:03:55 -0400

    Three.js implementation done

--- Three.js/main.js
+++ Three.js/main.js
@@ -1,8 +1,38 @@
 import * as THREE from 'three';
 
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
+scene.background = new THREE.Color("#36454f")
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BufferGeometry();
+const vertices = new Float32Array(
+    [
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0, 0.5, 0.0
+    ]
+);
+const colors = new Float32Array(
+    [
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0
+    ]
+);
+
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3, false));
+
+const material = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+});
+material.vertexColors = true;
+
+const triangle = new THREE.Mesh(geometry, material);
+scene.add(triangle);
+camera.position.z = 1;
+renderer.render(scene, camera);
